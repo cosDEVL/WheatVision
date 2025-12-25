864ffbc36fc19f7bf9fb9f51351756d3568328e1
Kozen <cos.dev.01@gmail.com>
2025-12-25 00:08:55 +0000

    LineController & BarController added to Chart.js import and register. Small changes to the backend trying to prevent some errors.

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -7,37 +7,34 @@ const notFound = (req, res, next) => {
     const error = new Error(`Not Found - ${req.originalUrl}`);
     res.status(404).json({ message: "Resource not found..." });
     next(error); // Passa l'errore al gestore di errori successivo
-}
-
+};
 
 /**
  * Middelware per la gestione di tutti gli errori.
  * Riceve gli errori passati da "next(error)"
  */
 const errorHandler = (err, req, res, next) => {
-    
     console.error(err);
-    
+
     //Questo gestisce gli errori operazionali lanciati da AppError
-    if (err.isOperational) { 
-        return res.status(err.statusCode).json({ message: err.message })
+    if (err.isOperational) {
+        return res.status(err.statusCode).json({ message: err.message });
     }
 
-    if (err.name === 'ValidationError') {
-
+    if (err.name === "ValidationError") {
         // Estrazione i messaggi d'errore in un array
-        const errors = Object.values(err.errors).map(e => e.message);
+        const errors = Object.values(err.errors).map((e) => e.message);
 
         // Unisce tutti i messaggi in una singola stringa, separati da una virgola
-        const message = `Errore nella validazione del Form: ${errors.join(', ')}`;
+        const message = `Errore nella validazione del Form: ${errors.join(
+            ", "
+        )}`;
         return res.status(400).json({ message });
-
     }
-
 
     // Determina lo status code
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     res.status(statusCode).json({ message: err.message });
-}
+};
 
 module.exports = { notFound, errorHandler };
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Chart as ChartJS, BarElement, RadialLinearScale, ArcElement, CategoryScale, LinearScale, PointElement, LineElement, Filler, Tooltip, Legend, Title, SubTitle } from 'chart.js';
+import { Chart as ChartJS, BarElement, RadialLinearScale, ArcElement, CategoryScale, LinearScale, PointElement, LineElement, LineController, BarController, Filler, Tooltip, Legend, Title, SubTitle } from 'chart.js';
 
 import HomePage from './pages/HomePage';
 import Form from './pages/Form';
 import DashBoard from './pages/DashBoard';
 import Report from './pages/Report';
 import NotFound from './pages/NotFound';
-ChartJS.register(BarElement, RadialLinearScale, ArcElement, CategoryScale, LinearScale, PointElement, LineElement, Filler, Tooltip, Legend);
+ChartJS.register(BarElement, RadialLinearScale, ArcElement, CategoryScale, LinearScale, PointElement, LineElement, LineController, BarController, Filler, Tooltip, Legend);
 
 function App() {
 
