49b8b71fdc488da1c7e3b64e48e4fdb633406932
Kozen <cos.dev.01@gmail.com>
2025-12-25 17:28:40 +0000

    fixed issue on errorHandler Middleware, preventing ERR_HTTP_HEADERS_SENT

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -5,7 +5,7 @@
 
 const notFound = (req, res, next) => {
     const error = new Error(`Not Found - ${req.originalUrl}`);
-    res.status(404).json({ message: "Resource not found..." });
+    res.status(404);
     next(error); // Passa l'errore al gestore di errori successivo
 };
 
